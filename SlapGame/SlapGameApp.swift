b54bc15193d5dc68169bd06653b07c8336985cf0
Gabriel Plutarco <gplutarco02@gmail.com>
2024-11-05 09:13:11 -0300

    Initial Commit

---
+++ SlapGame/SlapGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlapGameApp.swift
+//  SlapGame
+//
+//  Created by Gabriel Oliveira Plutarco on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SlapGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
