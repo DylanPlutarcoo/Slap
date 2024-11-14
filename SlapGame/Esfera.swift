df2ee9348a29b5640486c2fe610611420d83dcd5
viniciusgjusta <viniciusxime@gmail.com>
2024-11-13 23:06:16 -0300

    corrected spheres position

--- SlapGame/Esfera.swift
+++ SlapGame/Esfera.swift
@@ -16,73 +16,52 @@ struct ShapesView: View {
     @State private var changeSphere1 = false
     @State private var changeSphere2 = false
     
-    let yellowMaterial = SimpleMaterial(color: .yellow, isMetallic: true)
-    let redMaterial = SimpleMaterial(color: .red, isMetallic: true)
-    //    @State var position: SIMD3<Float>? = nil
-    var body: some View {
-        HStack(spacing: 25) {
-            RealityView { content in
-                let sphere1 = ModelEntity()
-                let appearenceSphere1 = ModelComponent(
-                    mesh: .generateSphere(radius: 0.3),
-                    materials: [redMaterial]
-                )
-                sphere1.components.set(appearenceSphere1)
-                sphere1.components.set(InputTargetComponent())
-                sphere1.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.3)]))
-                content.add(sphere1)
-            } update: { content in
-                if let model = content.entities.first {
-                    if changeSphere1 == true {
-                        model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [yellowMaterial]))
-                    } else {
-                        model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [redMaterial]))
-                    }
-                }
-            }
-            .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-                changeSphere1.toggle()
-            })
-            RealityView { content in
-                let sphere2 = ModelEntity()
-                let appearenceSphere2 = ModelComponent(
-                    mesh: .generateSphere(radius: 0.3),
-                    materials: [yellowMaterial]
-                )
-                // Habilita interações com a Entidade
-                sphere2.components.set(appearenceSphere2)
-                sphere2.components.set(InputTargetComponent())
-                sphere2.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.3)]))
-                content.add(sphere2)
-            } update: { content in
-                if let model = content.entities.first {
-                    if changeSphere2 == true {
-                        model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [yellowMaterial]))
-                    } else {
-                        model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [redMaterial]))
-                    }
-                }
-            }
-            .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-                changeSphere2.toggle()
-            })
-        }
-    }
+    let yellowMaterial = SimpleMaterial(color: .yellow, isMetallic: false)
+    let redMaterial = SimpleMaterial(color: .red, isMetallic: false)
     
-    // Positions and adds all geometric shapes to the scene.
-    //    func addGeometryShapes(to content: RealityViewContent) {
-    //        / An array of all the 3D entities representing different shapes.
-    //        let allGeometryEntities = [
-    //            ShapesView.createSphereEntity(),
-    //            ShapesView.createSphereEntity()
-    //        ]
-    //
-    //        //let xOffsets: [Float] = [/*-0.1, 0.1, 0*/0.5, 2, 0.5] // Ajuste esses valores para centralizar as esferas
-    //
-    //                // Define as posições e adiciona as esferas ao conteúdo
-    //        for (/*index, */entity) in allGeometryEntities.enumerated() {
-    //                   entity.position.x = xOffsets[index]
-    //            content.add(entity)
-    //        }
-    //    }
+    var body: some View {
+        RealityView { content in
+            let ballMesh = MeshResource.generateSphere(radius: 0.3)
+            
+            let sphere1 = ModelEntity(mesh: ballMesh,materials: [redMaterial])
+            sphere1.components.set(InputTargetComponent())
+            sphere1.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.3)]))
+            sphere1.position = SIMD3(x: -0.5, y: 1.0, z: -1.5)
+            content.add(sphere1)
+            
+            let sphere2 = ModelEntity(mesh: ballMesh, materials: [yellowMaterial])
+            sphere2.components.set(InputTargetComponent())
+            sphere2.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.3)]))
+            sphere2.position = SIMD3(x: 0.5, y: 1.0, z: -1.5)
+            content.add(sphere2)
+        } update: { content in
+            if let sphere1 = content.entities.first {
+                sphere1.components.set(ModelComponent(
+                    mesh: .generateSphere(radius: 0.3),
+                    materials: [changeSphere1 ? yellowMaterial : redMaterial])
+                )
+            }
+            if content.entities.count > 1{
+                let sphere2 = content.entities[1]
+                sphere2.components.set(ModelComponent(
+                    mesh: .generateSphere(radius: 0.3),
+                    materials: [changeSphere2 ? yellowMaterial : redMaterial]
+                ))
+            }
+        }
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { entity in
+            if entity.entity.position.x < 0 {
+                changeSphere1.toggle()
+            } else if entity.entity.position.x > 0 {
+                changeSphere2.toggle()
+            }
+        })
+//            if let model = content.entities.first {
+//                if changeSphere1 == true {
+//                    model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [yellowMaterial]))
+//                } else {
+//                    model.components.set(ModelComponent(mesh: .generateSphere(radius: 0.3), materials: [redMaterial]))
+//                }
+//            }
+    }
 }
--- SlapGame/SlapGameApp.swift
+++ SlapGame/SlapGameApp.swift
@@ -25,16 +25,15 @@ struct MyVisionApp: App {
         WindowGroup(id: WindowName.start.realName) {
             Inicio()
         }
-//        .defaultSize(width: 3, height: 3, depth: 3, in: .meters)
         .defaultSize(width: 990, height: 720)
         
         WindowGroup(id: WindowName.pause.realName) {
             PauseView()
         }.defaultSize(width: 577, height: 582)
-        
         ImmersiveSpace(id: WindowName.immersiveSpace.realName) {
             ShapesView()
         }
+//        .defaultSize(width: 3, height: 3, depth: 3, in: .meters)
     }
 }
 
